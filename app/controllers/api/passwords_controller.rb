4a7cb585282d9a7cf29a64ef712fb09f92c5f768
weyewe <w.yunnal@gmail.com>
2015-01-26 09:18:03 +0700

    disable password update

--- app/controllers/api/passwords_controller.rb
+++ app/controllers/api/passwords_controller.rb
@@ -5,19 +5,19 @@ class Api::PasswordsController < Api::BaseApiController
   def update
     @user = current_user
 
-    if @user.update_with_password(params[:user])
-      sign_in(@user, :bypass => true)
-      flash[:notice] = "Password is updated successfully."
-    else
-      flash[:error] = "Fail to update password. Check your input!"
-    end
+    # if @user.update_with_password(params[:user])
+    #   sign_in(@user, :bypass => true)
+    #   flash[:notice] = "Password is updated successfully."
+    # else
+    #   flash[:error] = "Fail to update password. Check your input!"
+    # end
     
     success_value = @user.errors.size == 0 
     
     if @user.errors.size == 0 
       render :json => {
         :success => true, 
-        :message => "Pasword is updated succsesfully",
+        :message => "Password is updated succsesfully",
         :auth_token => @user.authentication_token 
       }
     else
